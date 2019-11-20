fbe0e30bea7b9cd35dcbd3b30719bd397fd88fe7
Carlos Osorio <carlos.osorio005@gmail.com>
2019-11-19 19:53:24 -0500

    assignment progress

--- starter-code/src/assessment1.js
+++ starter-code/src/assessment1.js
@@ -1,36 +1,59 @@
 // Write a function that returns the product of 2 numbers
 function product(x, y) {
-  //
+  return x * y;
 }
 
 // Write a function that returns whether a given number is even
 function isEven(num) {
   //
+  if (num / 2 == 9) return true;
+  else return false;
 }
 
 // Return the largest of 2 numbers
 function maxOfTwoNumbers(a, b) {
-  //
+  if (a > b) return a
+  else return b
 }
 
 // Return the largest of 3 numbers
 function maxOfThreeNumbers(a, b, c) {
-  //
+  if ( a > b && a > c) return a
+  else if (b > a && b > c) return b
+  else return c
 }
 
 // Calculate the sum of an array of numbers
 function sumArray(numbers) {
-  //
+  let sum = 0;
+  for (let elem of numbers) {
+    sum += elem;
+  }
+
+
+  return sum;
 }
 
 // Return the largest number of a non-empty array
 function maxOfArray(numbers) {
-  //
+  let maxNumber = numbers[0];
+
+  for (let oneNumber of numbers) {
+    if (oneNumber > maxNumber) maxNumber = oneNumber;
+  }
+
+  return maxNumber;
 }
 
 // Return the longest string in an array
 function longestString(strings) {
-  //
+  let longString = strings[0];
+  for (let oneWord of strings) {
+    if (oneWord.length > longString.length) longString = oneWord;
+  }
+
+  return longString;
+  
 }
 
 // Return whether a word is in an array
@@ -45,7 +68,8 @@ function findUnique(wordsArr) {
 
 // Get the fullName from the object { firstName: 'Tony', lastName: 'Stark'}
 function getFullName(personObj) {
-  //
+  
+ return(personObj.firstName + " " + personObj.lastName)
 }
 
 // Return the largest number in a two dimensional array
