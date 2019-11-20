ced796d4010483b7dd40dbda41c0b47f87e1054a
Stanley Fils <stanleyfils@Stanleys-MacBook-Pro.local>
2019-11-19 20:23:35 -0500

    working on Iterations 5 -11

--- starter-code/src/assessment1.js
+++ starter-code/src/assessment1.js
@@ -1,22 +1,36 @@
 // Write a function that returns the product of 2 numbers
 function product(x, y) {
-  //
+  return x * y;
 }
 
 // Write a function that returns whether a given number is even
 function isEven(num) {
-  //
+ if (num%2 == 0) {
+   return true;
+ } else {
+   return false;
+ }
 }
 
 // Return the largest of 2 numbers
 function maxOfTwoNumbers(a, b) {
-  //
+  if (a > b) {
+    return a;
+  } else {
+    return b;
+  }
 }
 
 // Return the largest of 3 numbers
 function maxOfThreeNumbers(a, b, c) {
-  //
-}
+  if ( a > b && a > c) {
+    return a;
+  } else if (b > a && b > c) {
+    return b;
+  } else {
+    return c;
+  }
+} 
 
 // Calculate the sum of an array of numbers
 function sumArray(numbers) {
@@ -45,7 +59,7 @@ function findUnique(wordsArr) {
 
 // Get the fullName from the object { firstName: 'Tony', lastName: 'Stark'}
 function getFullName(personObj) {
-  //
+//
 }
 
 // Return the largest number in a two dimensional array
