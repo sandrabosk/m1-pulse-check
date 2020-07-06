56dd59768bc31bba1802324fd2a5f1a0d6043dc2
Nati <natalia.pupushi@gmail.com>
2020-07-06 06:12:04 -0300

    modulo1-check

--- starter-code/src/assessment1.js
+++ starter-code/src/assessment1.js
@@ -1,33 +1,60 @@
 // Write a function that returns the product of 2 numbers
 function product(x, y) {
-  //
+  return (x * y)
 }
 
+(product(2,4))
 // Write a function that returns whether a given number is even
 function isEven(num) {
-  //
-}
+
+    if (num % 2 == 0) {
+      return true
+    } else {
+      return false
+    }
+
+  }
 
 // Return the largest of 2 numbers
 function maxOfTwoNumbers(a, b) {
-  //
+  if(a>b){
+    return `El número más grande es ${a}`
+  } else if (a<b){
+    return `El número más grande es${b}`
+  } 
 }
+
+maxOfTwoNumbers(5,7)
 
 // Return the largest of 3 numbers
 function maxOfThreeNumbers(a, b, c) {
-  //
+  if(a>b){
+    return `El número más grande es ${a}`
+  } else if (a<b){
+    return `El número más grande es ${b}`
+  }
 }
 
 // Calculate the sum of an array of numbers
 function sumArray(numbers) {
-  //
+  var suma= 0;
+  for (var i = 0; i <= numbers.length; i++) {
+    suma += numbers[i];
+  } var resultado = sumArray(numbers)
+  return suma;
 }
+
+
+var numbers = [1, 2, 3, 4, 5];
+sumArray(numbers)
+console.log(resultado)
 
 // Return the largest number of a non-empty array
 function maxOfArray(numbers) {
-  //
-}
-
+    return Math.max.apply(null, numArray);
+  }
+var array = [2, 3, 4, 5]
+maxOfArray()
 // Return the longest string in an array
 function longestString(strings) {
   //
