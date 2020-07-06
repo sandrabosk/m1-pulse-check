9c5734e9890314d140f379d8a8bcd750ef5e183a
Sergi Botargues <sergibotargues@MacBook-Air-de-Sergi.local>
2020-07-06 11:11:01 +0200

    Sergi-Botargues (no acabado)

--- starter-code/src/assessment1.js
+++ starter-code/src/assessment1.js
@@ -1,35 +1,62 @@
 // Write a function that returns the product of 2 numbers
 function product(x, y) {
+  return (x * y);
   //
 }
 
 // Write a function that returns whether a given number is even
 function isEven(num) {
+  return (num % 2 == 0) ;
+    
+  }
   //
-}
+
 
 // Return the largest of 2 numbers
 function maxOfTwoNumbers(a, b) {
+
+  return Math.max(a,b);
   //
 }
 
 // Return the largest of 3 numbers
 function maxOfThreeNumbers(a, b, c) {
+
+  return Math.max(a,b,c);
   //
 }
 
 // Calculate the sum of an array of numbers
 function sumArray(numbers) {
+  if (numbers === 0) {
+    return 0;
+  }
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+  }
+  return sum;
+ 
   //
 }
 
 // Return the largest number of a non-empty array
-function maxOfArray(numbers) {
+  function maxOfArray(numbers) {
+    let largestNumber = 0;
+    for (let i = 0; i < numbers.length; i++) {
+      if (largestNumber < numbers[i]) {
+        largestNumber = numbers[i];
+      }
+    }
+    return largestNumber;
+    //
+  }
   //
-}
+
 
 // Return the longest string in an array
 function longestString(strings) {
+  
   //
 }
 
