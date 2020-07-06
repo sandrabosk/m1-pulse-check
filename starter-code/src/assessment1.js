6a7fc2e56c6c016d4fc41d2b46ae3fb0155610f6
Biel Querol <bielbes@hotmail.com>
2020-07-06 11:08:39 +0200

    pull

--- starter-code/src/assessment1.js
+++ starter-code/src/assessment1.js
@@ -1,27 +1,36 @@
 // Write a function that returns the product of 2 numbers
 function product(x, y) {
-  //
+  return x * y;
+  product;
 }
 
 // Write a function that returns whether a given number is even
-function isEven(num) {
-  //
-}
+//function isEven(num) {
+
+  //  if (num === % 2) {
+       // return `true`;
+   // } else {
+       // return`false`;
+//}
+ // }
+//} 
+
 
 // Return the largest of 2 numbers
 function maxOfTwoNumbers(a, b) {
-  //
+  return  (Math.max(a, b));
+
 }
 
 // Return the largest of 3 numbers
 function maxOfThreeNumbers(a, b, c) {
-  //
+  return (Math.max(a,b,c));
 }
 
 // Calculate the sum of an array of numbers
-function sumArray(numbers) {
-  //
-}
+   
+  
+    
 
 // Return the largest number of a non-empty array
 function maxOfArray(numbers) {
@@ -45,7 +54,8 @@ function findUnique(wordsArr) {
 
 // Get the fullName from the object { firstName: 'Tony', lastName: 'Stark'}
 function getFullName(personObj) {
-  //
+  return (firstName + lastName);
+  getFullName
 }
 
 // Return the largest number in a two dimensional array
