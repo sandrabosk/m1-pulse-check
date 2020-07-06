74bfa18120b0ba931bec316d5313e538a92a7ae5
Noiret <sergio.ko@icloud.com>
2020-07-06 11:02:42 +0200

    All mandatory specs done. Missing bonuses

--- starter-code/src/assessment1.js
+++ starter-code/src/assessment1.js
@@ -1,54 +1,76 @@
 // Write a function that returns the product of 2 numbers
 function product(x, y) {
-  //
+  return x * y;
 }
 
 // Write a function that returns whether a given number is even
 function isEven(num) {
-  //
+  if (num % 2 === 0) {
+    return true
+  } else {
+    return false
+  };
 }
 
 // Return the largest of 2 numbers
 function maxOfTwoNumbers(a, b) {
-  //
+  return Math.max(a, b);
 }
 
 // Return the largest of 3 numbers
 function maxOfThreeNumbers(a, b, c) {
-  //
+  return Math.max(a, b, c);
 }
 
 // Calculate the sum of an array of numbers
 function sumArray(numbers) {
-  //
+  if (numbers.length === 0) {
+    return 0;
+  }
+  return numbers.reduce((acc, val) => acc + val);
 }
 
 // Return the largest number of a non-empty array
 function maxOfArray(numbers) {
-  //
+  return Math.max.apply(null, numbers);
 }
 
 // Return the longest string in an array
 function longestString(strings) {
-  //
+  let longestWord = strings[0];
+  for (i = 0; i < strings.length; i++) {
+    if (strings[i].length > longestWord.length) {
+      longestWord = strings[i];
+    }
+  }
+  return longestWord;
 }
 
 // Return whether a word is in an array
 function doesWordExist(wordsArr, word) {
-  //
+  if (wordsArr.includes(word)) {
+    return true;
+  } else {
+    return false;
+  };
 }
 
 // Finding the first non-duplicate (non-repeating) word in an array
 function findUnique(wordsArr) {
-  //
+  if (wordsArr.length === 0) {
+    return false
+  };
+  let unique = wordsArr.find(x => wordsArr.indexOf(x) === wordsArr.lastIndexOf(x));
+  return unique;
 }
 
 // Get the fullName from the object { firstName: 'Tony', lastName: 'Stark'}
 function getFullName(personObj) {
-  //
+  return `${personObj.firstName} ${personObj.lastName}`
 }
 
 // Return the largest number in a two dimensional array
 function maxTwoDimArray(matrix) {
-  //
+  let max = Math.max(...[].concat(...matrix));
+  return max;
 }
