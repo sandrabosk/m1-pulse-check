8083450426fa3099d0c7bcbc93533e699188740d
Sergio <sccapineti@gmail.com>
2020-10-23 13:20:11 +0200

    not completemodule 1

--- starter-code/src/assessment1.js
+++ starter-code/src/assessment1.js
@@ -1,38 +1,51 @@
 // Write a function that returns the product of 2 numbers
 function product(x, y) {
-  //
+  let product = x * y;
+  return product;
 }
 
 // Write a function that returns whether a given number is even
 function isEven(num) {
-  //
+  if (num % 2 == 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 // Return the largest of 2 numbers
 function maxOfTwoNumbers(a, b) {
-  //
+  return Math.max(a, b);
 }
 
 // Return the largest of 3 numbers
 function maxOfThreeNumbers(a, b, c) {
-  //
+  return Math.max(a, b, c);
 }
 
 // Calculate the sum of an array of numbers
-function sumArray(numbers) {
-  //
-}
+//function sumArray(numbers) {
+//const sum=sumArrray.reduce(total,number) =>{
+//  total+number,0);});
+//}
+
+//}
 
 // Return the largest number of a non-empty array
-function maxOfArray(numbers) {
-  //
-}
+//function maxOfArray(numbers) {
+//var Total = numbers.reduce(function(prev, cur) {
+// return prev + cur;
+//}, 0);
+//return TotalmaxOfArray;
+//}
 
 // Return the longest string in an array
 function longestString(strings) {
-  //
+  var longestWord = strings.split(" ").sort(function (a, b) {
+    return b.length - a.length;
+  });
+  return longestWord[0].length;
 }
-
 // Return whether a word is in an array
 function doesWordExist(wordsArr, word) {
   //
