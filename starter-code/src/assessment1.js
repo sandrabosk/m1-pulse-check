07261cf4744044eaba31bdb1448f55d431d18b0e
Helene Stephan <helene.stephan@protonmail.com>
2020-10-23 11:34:59 +0200

    Ok, I know that I need to check not only isNaN but also != null

--- starter-code/src/assessment1.js
+++ starter-code/src/assessment1.js
@@ -1,24 +1,32 @@
 // Write a function that returns the product of 2 numbers
 function product(x, y) {
-  if (isNaN(x) || isNaN(y)) return false;
+  if (isNaN(x) || isNaN(y) || x === null || y === null) return false;
   return x * y;
 }
 
 // Write a function that returns whether a given number is even
 function isEven(num) {
-  if (isNaN(num)) return false;
+  if (isNaN(num) || num === null) return false;
   return !(num % 2);
 }
 
 // Return the largest of 2 numbers
 function maxOfTwoNumbers(a, b) {
-  if (isNaN(a) || isNaN(b)) return false;
+  if (isNaN(a) || isNaN(b) || a === null || b === null) return false;
   return a > b ? a : b;
 }
 
 // Return the largest of 3 numbers
 function maxOfThreeNumbers(a, b, c) {
-  if (isNaN(a) || isNaN(b) || isNaN(c)) return false;
+  if (
+    isNaN(a) ||
+    isNaN(b) ||
+    isNaN(c) ||
+    a === null ||
+    b === null ||
+    c === null
+  )
+    return false;
   return a > b ? (a > c ? a : c) : b > c ? b : c;
 }
 
@@ -28,7 +36,7 @@ function sumArray(numbers) {
   if (!Array.isArray(numbers)) return false;
   let onlyNum = true;
   let total = numbers.reduce(function (tot, num) {
-    if (isNaN(num)) onlyNum = false;
+    if (isNaN(num) || num === null) onlyNum = false;
     return tot + num;
   }, 0);
   if (!onlyNum) return false;
