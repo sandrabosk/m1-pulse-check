245d0484e26092f7c70a055b7be61208de0e2d86
Murkhunting <marcmp.arq@gmail.com>
2020-10-23 11:44:20 +0200

    almost done

--- starter-code/src/assessment1.js
+++ starter-code/src/assessment1.js
@@ -1,54 +1,39 @@
-// Write a function that returns the product of 2 numbers
-function product(x, y) {
-  //
-}
-
-// Write a function that returns whether a given number is even
-function isEven(num) {
-  //
-}
-
-// Return the largest of 2 numbers
-function maxOfTwoNumbers(a, b) {
-  //
-}
-
-// Return the largest of 3 numbers
-function maxOfThreeNumbers(a, b, c) {
-  //
-}
-
-// Calculate the sum of an array of numbers
-function sumArray(numbers) {
-  //
-}
-
-// Return the largest number of a non-empty array
-function maxOfArray(numbers) {
-  //
-}
-
-// Return the longest string in an array
-function longestString(strings) {
-  //
-}
-
-// Return whether a word is in an array
 function doesWordExist(wordsArr, word) {
-  //
+  return wordsArr.includes(word);
 }
 
 // Finding the first non-duplicate (non-repeating) word in an array
 function findUnique(wordsArr) {
-  //
+  if (wordsArr.length === 0) {
+    return false;
+  }
+  let nonRepited = wordsArr.filter((word, index) => {
+    return wordsArr.indexOf(word, index + 1) === -1;
+  });
+  return nonRepited[0];
 }
 
 // Get the fullName from the object { firstName: 'Tony', lastName: 'Stark'}
 function getFullName(personObj) {
-  //
+  const { firstName, lastName } = personObj;
+
+  return firstName + " " + lastName;
 }
 
 // Return the largest number in a two dimensional array
 function maxTwoDimArray(matrix) {
-  //
+  let maxNumbers1 = matrix.map((numbers) => {
+    numbers.sort(function (a, b) {
+      return b - a;
+    });
+    return numbers;
+  });
+
+  let maxNumbers2 = maxNumbers1.map((arr) => {
+    arr.sort(function (a, b) {
+      return b - a;
+    });
+  });
+
+  return maxNumbers2[0];
 }
