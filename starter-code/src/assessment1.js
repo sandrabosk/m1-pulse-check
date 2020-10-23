d053d31f781e4112956667fea37ee8dfa902fb42
Lucia Skudlik <lucia.skudlik@gmail.com>
2020-10-23 11:48:00 +0200

    done with advanced part-only one jasmine error

--- starter-code/src/assessment1.js
+++ starter-code/src/assessment1.js
@@ -1,5 +1,8 @@
 // Write a function that returns the product of 2 numbers
 function product(x, y) {
+  if (!x || !y) {
+    return false;
+  }
   return x * y;
 }
 
@@ -23,6 +26,9 @@ function maxOfTwoNumbers(a, b) {
 
 // Return the largest of 3 numbers
 function maxOfThreeNumbers(a, b, c) {
+  if (!a || !b || !c) {
+    return false;
+  }
   if (a > b && a > c) {
     return a;
   } else if (b > a && b > c) {
@@ -36,6 +42,14 @@ function maxOfThreeNumbers(a, b, c) {
 function sumArray(numbers) {
   if (!numbers) {
     return 0;
+  } else if (typeof numbers !== "object") {
+    return false;
+  } else if (
+    numbers.some((num) => {
+      return typeof num !== "number";
+    })
+  ) {
+    return false;
   }
   let sum = numbers.reduce((total, currentIndex) => {
     return total + currentIndex;
@@ -45,21 +59,35 @@ function sumArray(numbers) {
 
 // Return the largest number of a non-empty array
 function maxOfArray(numbers) {
+  if (numbers.length === 0 || typeof numbers !== "object") {
+    return false;
+  }
   return Math.max(...numbers);
 }
 
 // Return the longest string in an array
 function longestString(strings) {
-  let numArray = strings.map((word) => {
+  if (strings.length === 0 || typeof strings !== "object") {
+    return false;
+  }
+  let filteredArray = strings.filter((item) => {
+    return typeof item === "string";
+  });
+
+  let numArray = filteredArray.map((word) => {
     return word.length;
   });
+
   let biggestNum = Math.max(...numArray);
-  let indexOfLongestWord = numArray.indexOf(biggestNum);
+  let indexOfLongestWord = numArray.indexOf(biggestNum); //index within numArray
   return strings[indexOfLongestWord];
 }
 
 // Return whether a word is in an array
 function doesWordExist(wordsArr, word) {
+  if (typeof wordsArr !== "object") {
+    return false;
+  }
   let doesExist = wordsArr.some((item) => {
     return item === word;
   });
@@ -68,6 +96,9 @@ function doesWordExist(wordsArr, word) {
 
 // Finding the first non-duplicate (non-repeating) word in an array
 function findUnique(wordsArr) {
+  if (typeof wordsArr !== "object") {
+    return false;
+  }
   if (wordsArr.length === 0) {
     return false;
   }
@@ -79,13 +110,21 @@ function findUnique(wordsArr) {
 
 // Get the fullName from the object { firstName: 'Tony', lastName: 'Stark'}
 function getFullName(personObj) {
-  if (personObj.firstName && personObj.lastName) {
-    return `${personObj.firstName} ${personObj.lastName}`;
+  if (typeof personObj !== "object") {
+    return false;
   }
+  if (!personObj.firstName || !personObj.lastName) {
+    return false;
+  }
+
+  return `${personObj.firstName} ${personObj.lastName}`;
 }
 
 // Return the largest number in a two dimensional array
 function maxTwoDimArray(matrix) {
+  if (typeof matrix !== "object" || matrix.length === 0) {
+    return false;
+  }
   let maxNumbers = matrix.map((arr) => {
     return Math.max(...arr);
   });
