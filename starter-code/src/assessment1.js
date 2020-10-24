4c1759fd3df6340a77ed15e656a5836fd3e74a0b
Lucy Quinn <lucy.quinn.uk@gmail.com>
2020-10-24 18:39:49 +0200

    ccomplete more questions

--- starter-code/src/assessment1.js
+++ starter-code/src/assessment1.js
@@ -1,91 +1,85 @@
-// Write a function that returns the product of 2 numbers
-function product(x, y) {
-  return x * y;
-}
+// // Write a function that returns the product of 2 numbers
+// function product(x, y) {
+//   return x * y;
+// }
 
-// Write a function that returns whether a given number is even
-function isEven(num) {
-  return num % 2 === 0;
-}
+// // Write a function that returns whether a given number is even
+// function isEven(num) {
+//   return num % 2 === 0;
+// }
 
-// Return the largest of 2 numbers
-function maxOfTwoNumbers(a, b) {
-  return Math.max(a, b);
-}
+// // Return the largest of 2 numbers
+// function maxOfTwoNumbers(a, b) {
+//   return Math.max(a, b);
+// }
 
-// Return the largest of 3 numbers
-function maxOfThreeNumbers(a, b, c) {
-  return Math.max(a, b, c);
+// // Return the largest of 3 numbers
+// function maxOfThreeNumbers(a, b, c) {
+//   return Math.max(a, b, c);
 
-}
+// }
 
-// Calculate the sum of an array of numbers
-function sumArray(numbers) {
-  let sum = 0;
+// // Calculate the sum of an array of numbers
+// function sumArray(numbers) {
+//   let sum = 0;
 
-  numbers.forEach(function (num) {
-    return sum += num;
-  })
-  return sum;
-}
+//   numbers.forEach(function (num) {
+//     return sum += num;
+//   })
+//   return sum;
+// }
 
-// Return the largest number of a non-empty array
-function maxOfArray(numbers) {
-  return Math.max(...numbers);
-}
+// // Return the largest number of a non-empty array
+// function maxOfArray(numbers) {
+//   return Math.max(...numbers);
+// }
 
-// Return the longest string in an array
+// // Return the longest string in an array
 
-function longestString(strings) {
-  let longestWord = strings[0];
-  strings.forEach(function (word) {
-    if (word.length > longestWord.length) {
-      longestWord = word
-    }
-  })
-  return longestWord
-}
+// function longestString(strings) {
+//   let longestWord = strings[0];
+//   strings.forEach(function (word) {
+//     if (word.length > longestWord.length) {
+//       longestWord = word
+//     }
+//   })
+//   return longestWord
+// }
 
-// // Return whether a word is in an array
+// // // Return whether a word is in an array
 // function doesWordExist(wordsArr, word) {
 //   return wordsArr.includes(word);
 
 // }
 
-// Finding the first non-duplicate (non-repeating) word in an array
-debugger
-function findUnique(wordsArr) {
-  let arrWords = '';
-  let longestWord = '';
-  for (let i = 0; i < wordsArr.length; i++) {
-    if (arrWords.indexOf(wordsArr[i]) === -1) {
-      arrWords = wordsArr[i];
-    } else {
-      longestWord = '';
-    }
-  }
-  return longestWord;
-}
-console.log(findUnique("foo", "bar", "foo", "baz", "bar", "fizz", "foo"))
+// // Finding the first non-duplicate (non-repeating) word in an array
+// function findUnique(wordsArr) {
+//   let uniqueWords = [];
+//   for (let i = 0; i < wordsArr.length; i++) {
+//     if (wordsArr.indexOf(wordsArr[i], i + 1) === -1) {
+//       uniqueWords.push(wordsArr[i]);
+//     }
+//   }
+//   return uniqueWords[0];
+// }
 
-// Get the fullName from the object { firstName: 'Tony', lastName: 'Stark'}
-function getFullName(personObj) {
-  return personObj.firstName + ' ' + personObj.lastName;
-}
+// // Get the fullName from the object { firstName: 'Tony', lastName: 'Stark'}
+// function getFullName(personObj) {
+//   return personObj.firstName + ' ' + personObj.lastName;
+// }
 
-// Return the largest number in a two dimensional array
-function maxTwoDimArray(matrix) {
-  let largestNumber = matrix[0][0];
-  for (let i = 0; i < matrix.length; i++) {
-    for (let j = 0; j < matrix[i].length; j++) {
-      console.log(matrix[i][j])
-      if (largestNumber < matrix[i][j]) {
-        largestNumber = matrix[i][j];
-      }
-    }
-  }
-  return largestNumber
-}
+// // Return the largest number in a two dimensional array
+// function maxTwoDimArray(matrix) {
+//   let largestNumber = matrix[0][0];
+//   for (let i = 0; i < matrix.length; i++) {
+//     for (let j = 0; j < matrix[i].length; j++) {
+//       if (largestNumber < matrix[i][j]) {
+//         largestNumber = matrix[i][j];
+//       }
+//     }
+//   }
+//   return largestNumber
+// }
 
 //Bonus Questions
 //iteration 1
@@ -113,12 +107,84 @@ function maxOfThreeNumbers(a, b, c) {
   if (!Number(a, b, c)) return false;
   return Math.max(a, b, c);
 }
-
 //iteration 5
 function sumArray(numbers) {
-  let sum = 0;
-  numbers.forEach(function (number) {
-    return sum += number;
-  });
-  return sum;
+
+  if (typeof numbers !== 'object') {
+    return false
+  } else if (numbers.some((num) => { return typeof num !== 'number' })) {
+    return false
+  } else if (numbers.length === 0) {
+    return 0
+  } else {
+    const sum = numbers.reduce(function (a, b) {
+      return a + b
+    });
+    return sum
+  }
+}
+
+//iteration 6
+
+function maxOfArray(numbers) {
+  if (numbers.length !== 0 && Array.isArray(numbers)) {
+    return Math.max(...numbers);
+  } else {
+    return false
+  }
+}
+
+//iteration 7
+function longestString(stringsArr) {
+  if (stringsArr.length === 0) return false
+  const longestStr = stringsArr.sort(function (total, el) {
+    return el.length - total.length;
+  })
+  return longestStr[0];
+
+}
+
+// // iteration 8
+function doesWordExist(wordsArr, word) {
+  if (wordsArr.length === 0) return false;
+
+  const chosenWord = word
+  const isExistingWord = wordsArr.filter(function (word) {
+    return word === chosenWord
+  })
+
+  return isExistingWord == chosenWord
+}
+
+
+// //iteration 9
+// findUnique(wordsArr)
+
+// //iteration 10
+// function getFullName(personObj) {
+
+
+//   return personObj.firstName + ' ' + personObj.lastName;
+
+
+// }
+// // }
+
+// //iteration 11
+function maxTwoDimArray(matrix) {
+  if (typeof matrix !== 'object') {
+    return false
+  } else if (matrix.length === 0) {
+    return false
+  } else {
+    let largestNumber = matrix[0][0];
+    for (let i = 0; i < matrix.length; i++) {
+      for (let j = 0; j < matrix[i].length; j++) {
+        if (largestNumber < matrix[i][j]) {
+          largestNumber = matrix[i][j];
+        }
+      }
+    }
+    return largestNumber
+  }
 }
